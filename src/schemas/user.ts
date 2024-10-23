6b0efdd8f264a4a7583f11dfc8d6ac17cd820d91
suiwave <184898096+suiwave@users.noreply.github.com>
2024-10-23 18:20:03 +0900

    少しずつやっていこう

--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -1,11 +1,21 @@
 import { z } from "zod";
 
 export const userSchema = z.object({
-    name: z.string().min(1).catch("名無しさん"),
-    email: z.string().email({ message: "" }),
-    password: z.string().regex(/a/),
-    confirmPassword: z.string().email(),
-    birthday: z.string(),
-    job: z.string(),
-    hobby: z.string(),
-});
+    name: z.string().min(1, "ユーザー名は必須です").catch("名無しさん"),
+    email: z.string().email("メールアドレス形式で入力してください"),
+    // password: z.string()
+    //     .min(8, "パスワードは8文字以上である必要があります")
+    //     .max(100, "パスワードは100文字以下である必要があります")
+    //     .regex(
+    //         /^(?=.*[a-z])(?=.*[A-Z])[A-Za-z]+$/,
+    //         "パスワードには小文字と大文字が少なくとも1つずつ含まれている必要があります"
+    //     ),
+    // confirmPassword: z.string(),
+    // birthday: z.string().date(),
+    // job: z.string().catch(""),
+    // hobby: z.string().catch(""),
+})
+// .refine((data) => data.password === data.confirmPassword, {
+//     message: "入力されたパスワードと確認用パスワードが一致していません",
+//     path: ["confirmPassword"],
+// });;
