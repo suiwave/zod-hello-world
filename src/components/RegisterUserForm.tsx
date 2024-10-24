bad912a6b9912f83aa22277412a1dc545767212b
suiwave <184898096+suiwave@users.noreply.github.com>
2024-10-24 19:39:42 +0900

    user更新

--- src/components/RegisterUserForm.tsx
+++ src/components/RegisterUserForm.tsx
@@ -14,15 +14,61 @@ export default function RegisterUserForm() {
     });
 
     const onSubmit: SubmitHandler<User> = data => console.log(data);
-    const onInvalid: SubmitErrorHandler<User> = () => console.log(errors);
+    const onInvalid: SubmitErrorHandler<User> = () => console.log('errors', errors); // zodで定義したkeyが存在しない場合はinvalid
 
     return (
         <>
             <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
-                <div className='grid grid-cols-1'>
+                <div className='grid grid-cols-1 gap-2'>
                     <InputElement registration={register('name')} label={'ユーザー名'} errorMessage={errors.name?.message} />
                     <InputElement registration={register('email')} label={'メールアドレス'} errorMessage={errors.email?.message} />
-                    <input type="submit" />
+                    <InputElement type='password' registration={register('password')} label={'パスワード'} errorMessage={errors.password?.message} />
+                    <InputElement type='password' registration={register('confirmPassword')} label={'パスワード（確認用）'} errorMessage={errors.confirmPassword?.message} />
+                    <div>
+                        <select
+                            {...register("birthday.year", {
+                                valueAsNumber: true  // 文字列から数値に変換
+                            })}
+                        >
+                            <option value="">年</option>
+                            {Array.from({ length: 100 }, (_, i) => {
+                                const year = new Date().getFullYear() - i;
+                                return (
+                                    <option key={year} value={year}>
+                                        {year}
+                                    </option>
+                                );
+                            })}
+                        </select>
+                        <select
+                            {...register("birthday.month", {
+                                valueAsNumber: true
+                            })}
+                        >
+                            <option value="">月</option>
+                            {Array.from({ length: 12 }, (_, i) => (
+                                <option key={i + 1} value={i + 1}>
+                                    {i + 1}
+                                </option>
+                            ))}
+                        </select>
+                        <select
+                            {...register("birthday.day", {
+                                valueAsNumber: true
+                            })}
+                        >
+                            <option value="">日</option>
+                            {Array.from({ length: 31 }, (_, i) => (
+                                <option key={i + 1} value={i + 1}>
+                                    {i + 1}
+                                </option>
+                            ))}
+                        </select>
+                        {errors.birthday?.root?.message && (
+                            <p className="mt-1 rounded p-1 text-sm text-red-500 bg-red-100">{errors.birthday?.root?.message}</p>
+                        )}
+                    </div>
+                    <button className='rounded-full bg-slate-400 p-2 hover:opacity-70' type="submit">submit</button>
                 </div>
             </form>
         </>
--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -3,21 +3,39 @@ import { z } from "zod";
 export const user = z.object({
     name: z.string().min(1, "ユーザー名は必須です"),
     email: z.string().email("メールアドレス形式で入力してください"),
-    // password: z.string()
-    //     .min(8, "パスワードは8文字以上である必要があります")
-    //     .max(100, "パスワードは100文字以下である必要があります")
-    //     .regex(
-    //         /^(?=.*[a-z])(?=.*[A-Z])[A-Za-z]+$/,
-    //         "パスワードには小文字と大文字が少なくとも1つずつ含まれている必要があります"
-    //     ),
-    // confirmPassword: z.string(),
-    // birthday: z.string().date(),
+    password: z.string()
+        .min(8, "パスワードは8文字以上である必要があります")
+        .max(100, "パスワードは100文字以下である必要があります")
+        .regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$/,
+            "パスワードには小文字、大文字、数字が少なくとも1つずつ含まれている必要があります"
+        ),
+    confirmPassword: z.string(),
+    birthday: z.object({
+        year: z.number(),
+        month: z.number().min(1).max(12),
+        day: z.number().min(1).max(31)
+    }).refine(
+        (data) => {
+            // 存在しない日付の場合、存在する日付に変換されて返却される
+            // new Date(1990, 0, 32) === new Date(1990, 1, 1)
+            const date = new Date(data.year, data.month - 1, data.day);
+            return (
+                date.getFullYear() === data.year &&
+                date.getMonth() === data.month - 1 &&
+                date.getDate() === data.day
+            );
+        },
+        {
+            message: "存在しない日付です"
+        }
+    ),
     // job: z.string().catch(""),
     // hobby: z.string().catch(""),
 })
-// .refine((data) => data.password === data.confirmPassword, {
-//     message: "入力されたパスワードと確認用パスワードが一致していません",
-//     path: ["confirmPassword"],
-// });;
+    .refine((data) => data.password === data.confirmPassword, {
+        message: "入力されたパスワードと確認用パスワードが一致していません",
+        path: ["confirmPassword"],
+    });;
 
 export type User = z.infer<typeof user>;
