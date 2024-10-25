a7551f27321078f5967137494f846df0cd3e3d97
suiwave <184898096+suiwave@users.noreply.github.com>
2024-10-25 14:13:59 +0900

    refineのやつ解決。ラジオとオプションも実装できた。いい感じなのでcloseする

--- src/components/RegisterUserForm.tsx
+++ src/components/RegisterUserForm.tsx
@@ -1,7 +1,7 @@
 import { SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 
-import { User, user } from "../schemas"
+import { User, user, genderOptions } from "../schemas"
 import InputElement from './InputElement';
 import InputBirthDay from './InputBirthDay';
 import { InputRadioGroupElement } from './InputRadioGroupElement';
@@ -17,7 +17,7 @@ export default function RegisterUserForm() {
     });
 
     // valid時はparse後の値がgetValuesで取得できる。例えばbirthdayはnumber
-    const onSubmit: SubmitHandler<User> = data => console.log(data);
+    const onSubmit: SubmitHandler<User> = data => console.log('success', data);
 
     // zodで定義したkeyが存在しない場合はinvalid
     // invalid時はparse前の値がgetValuesで取得できる。例えばbirthdayはstring
@@ -28,16 +28,16 @@ export default function RegisterUserForm() {
             <form onSubmit={handleSubmit(onSubmit, onInvalid)}>
                 <div className='grid grid-cols-1 gap-2'>
                     <InputElement registration={register('name')} label={'ユーザー名'} errorMessage={errors.name?.message} />
-                    <InputRadioGroupElement label='性別' errorMessage={errors.gender?.message} registration={register('gender')} options={[]} />
-                    <InputElement registration={register('email')} label={'メールアドレス'} errorMessage={errors.email?.message} />
-                    <InputElement type='password' registration={register('password')} label={'パスワード'} errorMessage={errors.password?.message} />
-                    <InputElement type='password' registration={register('confirmPassword')} label={'パスワード（確認用）'} errorMessage={errors.confirmPassword?.message} />
+                    <InputRadioGroupElement label='性別' errorMessage={errors.gender?.message} registration={register('gender')} options={genderOptions} />
                     <InputBirthDay label='生年月日'
                         registrationYear={register("birthday.year")}
                         registrationMonth={register("birthday.month")}
                         registrationDay={register("birthday.day")}
                         errorMessage={errors.birthday?.root?.message}
                     />
+                    <InputElement registration={register('email')} label={'メールアドレス'} errorMessage={errors.email?.message} />
+                    <InputElement type='password' registration={register('password')} label={'パスワード'} errorMessage={errors.password?.message} />
+                    <InputElement type='password' registration={register('confirmPassword')} label={'パスワード（確認用）'} errorMessage={errors.confirmPassword?.message} />
                     <button className='rounded-full bg-slate-400 p-2 hover:opacity-70' type="submit">submit</button>
                 </div>
             </form>
--- src/schemas/user.ts
+++ src/schemas/user.ts
@@ -7,18 +7,11 @@ const stringToNumber = (schema: z.ZodNumber) =>
 
 export const user = z.object({
     name: z.string().min(1, "ユーザー名は必須です"),
-    gender: z.enum(['male', 'female'], {
-        required_error: '性別を選択してください',
-    }),
-    email: z.string().email("メールアドレス形式で入力してください"),
-    password: z.string()
-        .min(8, "パスワードは8文字以上である必要があります")
-        .max(100, "パスワードは100文字以下である必要があります")
-        .regex(
-            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$/,
-            "パスワードには小文字、大文字、数字が少なくとも1つずつ含まれている必要があります"
-        ),
-    confirmPassword: z.string(),
+    gender: z.preprocess(
+        (data) => data ? data : undefined,
+        z.enum(['male', 'female'], {
+            required_error: '性別を選択してください',
+        })),
     birthday: z.object({
         year: stringToNumber(z.number()),
         month: stringToNumber(z.number().min(1).max(12)),
@@ -38,7 +31,15 @@ export const user = z.object({
             message: "存在しない日付です"
         }
     ),
-    // hobby: z.string(),
+    email: z.string().email("メールアドレス形式で入力してください"),
+    password: z.string()
+        .min(8, "パスワードは8文字以上である必要があります")
+        .max(100, "パスワードは100文字以下である必要があります")
+        .regex(
+            /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).*$/,
+            "パスワードには小文字、大文字、数字が少なくとも1つずつ含まれている必要があります"
+        ),
+    confirmPassword: z.string(),
 })
     .refine((data) => data.password === data.confirmPassword, {
         message: "入力されたパスワードと確認用パスワードが一致していません",
@@ -49,4 +50,13 @@ export type User = z.infer<typeof user>;
 
 // genderを参照してラジオボタン用の配列をexportしたい
 // 全体をrefineすると以下の方法でアクセスできない。なぜ
-// const gender = user.shape.gender
+// https://github.com/colinhacks/zod/issues/2056
+// refine, transform, superRefine, and maybe a few others return ZodEffects, which wraps another ZodType. You can access that type with ._def.schema.
+const gender =
+    user._def.schema    // user._def.schema でrefineする前のschemaを取得
+        .shape.gender   // .shape でobjectの単体要素にアクセス
+        ._def.schema    // gender はpreprocessしてるので、同様に_def でpreprocessした後の中身を取得できた
+export const genderOptions = [
+    { label: '男性', value: gender.Enum.male },
+    { label: '女性', value: gender.Enum.female },
+];
